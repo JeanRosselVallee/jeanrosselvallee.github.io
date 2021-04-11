ecacd889b031393ffc2ddba317ec54c33ac72803
JeanRosselVallee <jv.isa4031@gmail.com>
2021-04-11 13:50:27 +0200

    add Jupyter sub-folders

--- jv_git.sh
+++ jv_git.sh
@@ -18,9 +18,11 @@ NB_SECONDS=` expr $NB_HOURS \* 3600 `
 # CMD_LIST="$CMD1 $CMD2 $CMD3"
 
 CMD0="git_config_credential.helper_'cache_--timeout=$NB_SECONDS'"
-CMD_LIST="	git_add_.  \
-			git_commit_-m_'$COMMENT' \
-			git_push "
+CMD_LIST="	
+	git_add_.  \
+	git_commit_-m_'$COMMENT' \
+	git_push \
+	"
 
 # SYNTAX
 echo "INFO Syntax: $0 \"Comment for commit\" "
@@ -48,9 +50,10 @@ if [ $NB_PARAMS -ne 1 ]
 fi
 
 # Launch git add -> commit -> push 
-echo "\nLaunch git add -> commit -> push :"
+
 for CMDn in $CMD_LIST
 	do
+		echo "==================================================="
 		CMD=`echo "     $CMDn" | sed "s/_/ /g"`
 		echo "$CMD"
 		eval $CMD
