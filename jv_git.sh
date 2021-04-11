3e777acd8a29729d4058e226d9aba79e9cb9b849
JeanRosselVallee <jv.isa4031@gmail.com>
2021-04-11 13:40:14 +0200

    add folder Jupyter

--- jv_git.sh
+++ jv_git.sh
@@ -1,7 +1,7 @@
 # Goal: push code changed by a single coder
 
 # Arguments
-COMMENT="$1"
+COMMENT=`echo "$1" |  sed "s/ /_/g" ` 
 NB_PARAMS=$#
 
 # Input Values
@@ -11,17 +11,33 @@ NB_HOURS=3
 NB_SECONDS=` expr $NB_HOURS \* 3600 ` 
 
 # Commands list
-CMD0="git config credential.helper 'cache --timeout=$NB_SECONDS'"
-CMD1="git add . "
-CMD2="git commit -m '$COMMENT' "
-CMD3="git push "
+# CMD0="git config credential.helper 'cache --timeout=$NB_SECONDS'"
+# CMD1="\"git add . \""
+# CMD2="git commit -m '$COMMENT' "
+# CMD3='git push '
+# CMD_LIST="$CMD1 $CMD2 $CMD3"
+
+CMD0="git_config_credential.helper_'cache_--timeout=$NB_SECONDS'"
+CMD_LIST="	git_add_.  \
+			git_commit_-m_'$COMMENT' \
+			git_push "
 
 # SYNTAX
 echo "INFO Syntax: $0 \"Comment for commit\" "
+
+NB_CRED_LINES=`git config --list | grep credential.helper | wc -l`
+if [ $NB_CRED_LINES -eq 0 ] 
+	then
+		CMD_LIST="	$CMD0 \
+					$CMD_LIST"
+	else
+		echo "Credentials timeout is set in seconds: $NB_SECONDS "
+fi
+
 echo "INFO Commands to be executed:"
-for CMDn in "$CMD0" "$CMD1" "$CMD2" "$CMD3"
+for CMDn in $CMD_LIST
 	do
-		echo "     $CMDn"
+		echo "     $CMDn" | sed "s/_/ /g"
 	done
 
 # Parameters Check
@@ -31,23 +47,18 @@ if [ $NB_PARAMS -ne 1 ]
 		exit 1 
 fi
 
-NB_CRED_LINES=`git config --list | grep credential.helper | wc -l`
-if [ $NB_CRED_LINES -eq 0 ] 
-	then
-		echo $CMD0
-		$CMD0
-	else
-		echo "Credentials timeout is set in seconds: $NB_SECONDS "
-fi
-
 # Launch git add -> commit -> push 
-for CMDn in "$CMD1" "$CMD2" "$CMD3"
+echo "\nLaunch git add -> commit -> push :"
+for CMDn in $CMD_LIST
 	do
-		echo "$CMDn"
-		eval $CMDn
+		CMD=`echo "     $CMDn" | sed "s/_/ /g"`
+		echo "$CMD"
+		eval $CMD
 		if [ $? -ne 0 ]
 			then 
 				echo "ERROR: Command interrupted"
 				exit 1
+			else
+				echo "OK: Command Completed"
 		fi
 	done
